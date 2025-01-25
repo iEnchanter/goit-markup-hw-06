e7d4f8edfbe70d0bc4d7b2dc2e82cac903b2d518
Sergii Gorodeikin <55316965+iEnchanter@users.noreply.github.com>
2025-01-25 21:31:46 +0100

    add file

---
+++ main.js
@@ -0,0 +1,43 @@
+const modal = document.querySelector('.backdrop');
+const openModalBtn = document.querySelector('.hero-btn');
+const closeModalBtn = document.querySelector('.modal-close-btn');
+const mobileModal = document.querySelector('.menu-open-btn');
+const mobileOpenBtn = document.querySelector('.mobile-burger-btn');
+const mobileCloseBtn = document.querySelector('.mobile-close-btn');
+
+openModalBtn.addEventListener('click', () => {
+    modal.classList.add('is-open');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+});
+
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+    modal.classList.remove('is-open');
+    }
+});
+
+mobileOpenBtn.addEventListener('click', () => {
+    mobileModal.classList.toggle('is-open');
+});
+
+mobileCloseBtn.addEventListener('click', () => {
+    mobileModal.classList.remove('is-open');
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 250) {
+    scrollBtn.style.display = 'flex';
+    } else {
+    scrollBtn.style.display = 'none';
+    }
+});
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+    });
+});
